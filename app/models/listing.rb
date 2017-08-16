5a61f6fd6742c0bf7f2df7151906f4921ca6b8fc
gabidavila <gabidavila@gmail.com>
2017-08-16 09:59:03 -0400

    added custom behaviors to listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,22 @@ class Listing < ActiveRecord::Base
   has_many :guests, :class_name => "User", :through => :reservations
 
   validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+
+  after_create :change_user_status
+  before_destroy :change_host_status_after_destroy
+
+  def average_review_rating
+    self.reviews.average(:rating)
+  end
+
+  def change_user_status
+    self.host.toggle!(:host)
+  end
+
+  def change_host_status_after_destroy
+    if Listing.all.where(host: self.host).where.not(id: self).empty?
+      self.host.update(host: false)
+    end
+  end
+
 end
