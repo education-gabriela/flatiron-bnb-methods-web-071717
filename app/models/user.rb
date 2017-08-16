13febde3477fff639ce98233e01578fe7d6e3a45
gabidavila <gabidavila@gmail.com>
2017-08-15 22:08:05 -0400

    changed to relationship queries with source

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,8 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
 
-  def guests
-    Reservation.joins(:listing).where(listings: {host_id: self}).map {|r| r.guest}
-  end
-
-  def hosts
-    Listing.joins(:reservations).where(reservations: {guest_id: self}).uniq.map {|l| l.host}
-  end
-
-  def host_reviews
-    Review.joins(reservation: :listing).where(listings: {host_id: self})
-  end
+  has_many :guests, :through => :reservations, :class_name => "User"
+  has_many :trips_listings, :through => :trips, :source => :listing
+  has_many :hosts, :through => :trips_listings, :foreign_key => :host_id
+  has_many :host_reviews, :through => :listings, :source => :reviews
 end
