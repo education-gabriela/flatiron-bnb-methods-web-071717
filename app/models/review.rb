4b16fd02eb3994ebba8a07cebee7a45814b40b21
gabidavila <gabidavila@gmail.com>
2017-08-15 21:31:56 -0400

    added reviews validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,16 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, :reservation, :description, presence: true
+  validate :accepted_reservation?, :checkout_happened?
+
+  def accepted_reservation?
+    self.errors.add :guest_id, "your reservation wasn't accepted" unless self.reservation && self.reservation.status == "accepted"
+  end
+
+  def checkout_happened?
+    if self.reservation && self.reservation.checkout >= Date.today
+      self.errors.add :guest_id, "not enough time to do a review"
+    end
+  end
 end
